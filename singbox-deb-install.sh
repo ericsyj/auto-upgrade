d4ceca083ae8801af4237fb22e3ecdf048d1e2d5
ericsyj <97294040+ericsyj@users.noreply.github.com>
2024-05-29 14:28:44 +0800

    Update singbox-deb-install.sh

--- singbox-deb-install.sh
+++ singbox-deb-install.sh
@@ -15,7 +15,7 @@ case "${ARCH_RAW}" in
     ;;
 esac
 
-VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases |
+VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest |
     grep tag_name |
     cut -d ":" -f2 |
     sed 's/\"//g;s/\,//g;s/\ //g;s/v//' |
